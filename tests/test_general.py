332d690cc810eb2c0fb80cddc385f11598c2833d
Tomas Poveda <tpovedatd@gmail.com>
2021-03-27 16:57:10 +0100

    Updated tests

--- tests/test_general.py
+++ tests/test_general.py
@@ -7,10 +7,8 @@ Module that contains general tests for tpDcc-libs-composite
 
 from tpDcc.libs.composite import __version__
 
-from tpDcc.libs.unittests.core import unittestcase
+import pytest
 
 
-class TestGeneral(unittestcase.UnitTestCase()):
-
-    def test_version(self):
-        assert __version__.get_version()
+def test_version(self):
+    assert __version__.get_version()
